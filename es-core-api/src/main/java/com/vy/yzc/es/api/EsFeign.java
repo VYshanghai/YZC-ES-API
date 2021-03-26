f640f8c0dd7fffb59bacec1d018e86cd7bdc1ddc
vikkowang <1218297052@qq.com>
2021-03-26 15:00:12 +0800

    feign的引入

--- es-core-api/src/main/java/com/vy/yzc/es/api/EsFeign.java
+++ es-core-api/src/main/java/com/vy/yzc/es/api/EsFeign.java
@@ -1,8 +1,17 @@
 package com.vy.yzc.es.api;
 
+import com.vy.yzc.es.dto.EsOffersSaveReq;
+import com.vy.yzc.es.dto.EsSearchVO;
+import com.vy.yzc.es.dto.OffersFilterReq;
+import com.vy.yzc.es.dto.OffersKeywordRecommendReq;
+import com.vy.yzc.es.dto.OffersNearReq;
+import com.vy.yzc.es.dto.OffersSearchReq;
 import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
@@ -10,13 +19,53 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @Date: 2021/3/26 01:48
  * @Description:
  */
-@FeignClient(value = "yzc-es", path = "/provider/es/offers")
+@FeignClient(value = "yzc-es-core", path = "/provider/es/offers")
 public interface EsFeign {
 
+	/**
+	 * 关键词联想
+	 * @param req
+	 * @return
+	 */
+	@PostMapping("/keyword/recommend")
+	EsSearchVO<String> keywordRecommend(@RequestBody OffersKeywordRecommendReq req);
 
-	@GetMapping("/update/deleted")
-	Boolean updateOffersEsDeletedStateAsync(@RequestParam("offersIds") List<Long> offersIds,
-			@RequestParam("deleted") Integer deleted);
+	/**
+	 * 根据关键词分页搜索
+	 */
+	@PostMapping("/search/keyword/{userId}")
+	EsSearchVO<Long> searchKeyword(@RequestBody OffersSearchReq req, @PathVariable(required = false)Long userId);
 
+	/**
+	 * 根据条件过滤筛选
+	 */
+	@PostMapping("/search/filter/{userId}")
+	EsSearchVO<Long> searchFilter(@RequestBody OffersFilterReq req, @PathVariable(required = false) Long userId);
+	
+
+	/**
+	 * 批量删除
+	 */
+	@PostMapping("/batch/delete")
+	Boolean batchDelete(@RequestBody List<Long> offersIds);
+
+
+	/**
+	 * 附近的爆料
+	 */
+	@PostMapping("/near/{userId}")
+	EsSearchVO<Long> near(@RequestBody OffersNearReq req, @PathVariable(required = false) Long userId);
+
+	/**
+	 * 更新deleted状态
+	 */
+	@PostMapping("/update/deleted/state")
+	Boolean updateDeletedState(@RequestBody List<Long> offersIds, @PathVariable(required = false) Integer deleted);
+
+	/**
+	 * 批量新增req
+	 */
+	@PostMapping("/save/reqs")
+	Boolean saveReqs(@RequestBody List<EsOffersSaveReq> reqs);
 
 }
